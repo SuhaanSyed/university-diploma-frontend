480a0df5c593ff2fb4d35bcd6745eb11b92f6cae
Suhaan Syed <67168522+SuhaanSyed@users.noreply.github.com>
2024-07-13 15:55:10 -0500

    connects to wallet and fetches address and balance

--- diploma-verification/src/App.js
+++ diploma-verification/src/App.js
@@ -1,78 +1,97 @@
-import React from "react";
-import "./App.css";
-import { useState, useEffect } from "react";
 import Web3 from "web3";
-function App() {
-  const [isConnected, setIsConnected] = useState(false);
-  const [ethBalance, setEthBalance] = useState("");
+import { useState } from "react";
+import { ethers } from "ethers";
+import { Card } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-  const detectCurrentProvider = () => {
-    let provider;
-    if (window.ethereum) {
-      provider = window.ethereum;
-    } else if (window.web3) {
-      provider = window.web3.currentProvider;
-    } else {
-      console.log("No Ethereum browser detected. Please install Metamask");
-    }
 
-    return provider;
-  };
+import "./App.css";
+import ConnectWalletButton from "./components/ConnectWalletButton";
+import mobileCheck from "./helpers/mobileCheck";
+import getLinker from "./helpers/deepLink";
 
-  const onConnect = async () => {
+const App = () => {
+  const [loading, setLoading] = useState(false);
+  const [address, setAddress] = useState("");
+  const [balance, setBalance] = useState("");
+
+
+  const onPressConnect = async () => {
+    setLoading(true);
+
     try {
-      const currentProvider = detectCurrentProvider();
-      if (currentProvider) {
-        await currentProvider.request({ method: "eth_requestAccounts" });
-        const web3 = new Web3(currentProvider);
-        const userAccount = await web3.eth.getAccounts();
-        const account = userAccount[0];
-        let ethBalance = await web3.eth.getBalance(account);
-        setEthBalance(ethBalance);
-        setIsConnected(true);
+      const yourWebUrl = "mysite.com"; // Replace with your website domain
+      const deepLink = `https://metamask.app.link/dapp/${yourWebUrl}`;
+      const downloadMetamaskUrl = "https://metamask.io/download.html";
+
+      const getbalance = (address) => {
+        // Requesting balance method
+        window.ethereum
+          .request({
+            method: "eth_getBalance",
+            params: [address, "latest"],
+          })
+          .then((balance) => {
+            // Setting balance
+            setBalance({
+              Balance:
+                ethers.utils.formatEther(balance),
+            });
+          });
+      };
+
+
+      if (window?.ethereum?.isMetaMask) {
+        // Desktop browser
+        const accounts = await window.ethereum.request({
+          method: "eth_requestAccounts",
+        });
+
+        const account = Web3.utils.toChecksumAddress(accounts[0]);
+        setAddress(account);
+        getbalance(account);
+      } else if (mobileCheck()) {
+        // Mobile browser
+        const linker = getLinker(downloadMetamaskUrl);
+        linker.openURL(deepLink);
+      } else {
+        window.open(downloadMetamaskUrl);
       }
-    } catch (err) {
-      console.error(err);
+    } catch (error) {
+      console.log(error);
     }
+
+
+
+
+
+    setLoading(false);
   };
 
-  const onDisconnect = () => {
-    setIsConnected(false);
-  };
+  const onPressLogout = () => {
+    setAddress("");
+    setBalance("");
+  }
 
   return (
     <div className="App">
-      <div className="app-header">
-        <h1>hello world!</h1>
-      </div>
-
-      <div className="app-wrapper">
-        {!isConnected && (
-          <div>
-            <button className="app-button_login" onClick={onConnect}>
-              Login
-            </button>
-          </div>
-        )}
-      </div>
-      {isConnected && (
-        <div className="app-wrapper">
-          <div className="app-details">
-            <h2> You are connected to Metamask </h2>
-            <div className="app-balance">
-              <span>Balance: </span>
-              {ethBalance}
-            </div>
-          </div>
-          <div>
-            <button className="app-button_logout" onClick={onDisconnect}>
-              Logout
-            </button>
-          </div>
-        </div>
-      )}
+      <header className="App-header">
+        <ConnectWalletButton
+          onPressConnect={onPressConnect}
+          onPressLogout={onPressLogout}
+          loading={loading}
+          address={address}
+          balance={balance}
+        />
+      </header>
+      <Card>
+        <Card.Body>
+          <Card.Title>Address: {address}</Card.Title>
+          <Card.Text>Balance: {balance.Balance}</Card.Text>
+        </Card.Body>
+      </Card>
     </div>
   );
-}
+};
 
 export default App;
